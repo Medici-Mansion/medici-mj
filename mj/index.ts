8a996fb92d2a8d23c5913631a28b23e8dfa97d5d
Raymond <akdfhr2@gmail.com>
2023-11-18 23:21:34 +0900

    feat: 미드저니 api 스트림 응답 구현
    - 다중 요청 응답 가능
    - Optinal<>: null | T 타입 생성 필요
    - 응답 객체 생성 필요

---
+++ mj/index.ts
@@ -0,0 +1,96 @@
+import Stream from "@elysiajs/stream";
+import { Midjourney } from "./midjourney";
+
+export * from "./midjourney";
+export * from "./discord.message";
+export * from "./discord.ws";
+export * from "./interfaces/index";
+export * from "./midjourne.api";
+export * from "./command";
+export * from "./verify.human";
+export * from "./banned.words";
+export * from "./face.swap";
+
+/**
+ *
+ * a simple example of using the imagine api with ws
+ * ```
+ * npx tsx example/imagine-ws.ts
+ * ```
+ */
+export async function main(stream: Stream<any>, prompt: string | null) {
+  const client = new Midjourney({
+    ServerId: <string>process.env.SERVER_ID,
+    ChannelId: <string>process.env.CHANNEL_ID,
+    SalaiToken: <string>process.env.SALAI_TOKEN,
+    HuggingFaceToken: <string>process.env.HUGGINGFACE_TOKEN,
+    Debug: true,
+    Ws: true, // required  `Only you can see this`
+  });
+  function getProgress(uri: string, progress: string) {
+    console.log("Imagine.loading", uri, "progress", progress);
+    console.log(uri, progress, "<<<<!=============================");
+    stream.send({ uri, progress });
+  }
+  await client.Connect(); // required
+  const image = await client.Imagine(
+    // "Describe a live-action battle sequence featuring Iron Man and another Marvel hero of your choice. --turbo",
+    `${prompt ?? "realize cutty cat like github icon"} --turbo`,
+    getProgress
+  );
+  client.Close();
+  if (image) {
+    stream.send(image);
+  }
+  stream.close();
+
+  // if (!Imagine) {
+  //   return;
+  // }
+  // const reroll = await client.Reroll({
+  //   msgId: <string>Imagine.id,
+  //   hash: <string>Imagine.hash,
+  //   flags: Imagine.flags,
+  //   loading: (uri: string, progress: string) => {
+  //     console.log("Reroll.loading", uri, "progress", progress);
+  //   },
+  // });
+  // console.log({ reroll });
+
+  // const Variation = await client.Variation({
+  //   index: 2,
+  //   msgId: <string>Imagine.id,
+  //   hash: <string>Imagine.hash,
+  //   flags: Imagine.flags,
+  //   loading: (uri: string, progress: string) => {
+  //     console.log("Variation.loading", uri, "progress", progress);
+  //   },
+  // });
+
+  // console.log({ Variation });
+  // if (!Variation) {
+  //   return;
+  // }
+  // const Upscale = await client.Upscale({
+  //   index: 2,
+  //   msgId: <string>Variation.id,
+  //   hash: <string>Variation.hash,
+  //   flags: Variation.flags,
+  //   loading: (uri: string, progress: string) => {
+  //     console.log("Upscale.loading", uri, "progress", progress);
+  //   },
+  // });
+  // console.log({ Upscale });
+}
+// main()
+//   .then(() => {
+//     // console.log("finished");
+//     // process.exit(0);
+//   })
+//   .catch((err) => {
+//     console.log("finished");
+//     console.error(err);
+//     process.exit(1);
+//   });
+
+console.log(process.env.SALAI_TOKEN, "<<process.env.HUGGINGFACE_TOKEN");
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,20 @@
 import { Elysia } from "elysia";
+import { main } from "../mj";
+import { Stream } from "@elysiajs/stream";
+const app = new Elysia()
+  .get("/", (context) => {
+    const { prompt } = context.query;
+    return new Stream(async (stream) => main(stream, prompt));
+  })
+  // .get('/', () => new Stream(async (stream) => {
+  //     stream.send('hello')
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+  //     await stream.wait(1000)
+  //     stream.send('world')
+
+  //     stream.close()
+  // }))
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
